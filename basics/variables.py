75904fc73379f44ec6360e0cf20ccde2260327c0
ben244294 <benjaminessibo89@gmail.com>
2026-04-19 14:29:46 +0000

    Add user input for age and school information

--- basics/variables.py
+++ basics/variables.py
@@ -1,1 +1,15 @@
-
+year_birth = int(input("Enter your year of birth"))
+current_year = int(input("Enter current year"))
+current_age = current_year - year_birth
+print("Your current age is",current_age)
+name = input("What is you name?")
+age = input("how old are you?")
+school = input("What school are you currently in?")
+print(" helooooo I'm " + name,"I'm " + age)
+iq = 190
+user_age = iq / 4
+print(user_age)
+a, b, c = 1, 5, 5.6
+print(a)
+print(b)
+print(c)

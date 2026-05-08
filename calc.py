090a446a233f50fe6467007753b71b1e92acbe09
Benjamin.E. Ofori <219198565+ben244294@users.noreply.github.com>
2026-05-08 19:53:45 +0000

    Add interactive computation loop in calc.py
    Implement a simple calculator that evaluates user input expressions and handles errors.

---
+++ calc.py
@@ -0,0 +1,11 @@
+while True : #loop forever unless something triggers the loop to terminate in this case once the computation is done or the user enters x
+    compute = input("Please enter your computation (or x tp quit):")
+    if compute == "x":
+        break
+    try:
+        result = eval(compute)
+        print(f"your result is {result}") 
+    except ZeroDivisionError: #error handling
+        print("Error: you cannot divide by zero")
+    except (ValueError, SyntaxError):
+        print("Error:Invalid expression entered")

dfcbc6ef0fce53ec06abdcfb19a487319dfa826c
Benjamin.E. Ofori <219198565+ben244294@users.noreply.github.com>
2026-06-05 14:14:03 +0000

    Implement continuous task entry loop
    Refactored task input and display logic into a loop to allow continuous task entry until user decides to quit.

--- mini-projects/to_do.py
+++ mini-projects/to_do.py
@@ -5,9 +5,19 @@
 #data structures to use , I'd use a list instead of a tuple to allow changes to be made easily
 #I could use a loop to , to iterate over and check if an activity has been done and also use .append method which belongs to strings
 
-task_list = input("Enter a list of tasks separated by commas")
-to_do_list = [task.strip() for task in task_list.split(",")]#.strip removes any accidental leading/trailing spaces,.strip converts a single input string into individual list items
-print (f"Your list of tasks are")
-for to_do in to_do_list :
-    print(f" -  {to_do}")
+#task_list = input("Enter a list of tasks separated by commas")
+#to_do_list = [task.strip() for task in task_list.split(",")]#.strip removes any accidental leading/trailing spaces,.strip converts a single input string into individual list items
+#print (f"Your list of tasks are")
+#for to_do in to_do_list :
+ #   print(f" -  {to_do}")
 
+while True:
+    task_list = input("Enter a list of tasks separated by commas")
+    to_do_list = [task.strip() for task in task_list.split(
+        ",")]  # .strip removes any accidental leading/trailing spaces,.strip converts a single input string into individual list items
+    print(f"Your list of tasks are")
+    for to_do in to_do_list:
+        print(f" -  {to_do}")
+    terminate = input("press x to quit or c to continue")
+    if terminate == "x":
+        break
